ebcd18f97d3e838a92c62f235b4c9cc1489519f9
Ruud0511 <post@ruud.co>
2023-07-05 19:02:07 +0200

    huiswerk waar ik nog niet veel van begrijp

--- loops.js
+++ loops.js
@@ -11,7 +11,7 @@
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
-let asterisk = 0;
+let asterisk = '*';
 
 for (let i = 0; i < 5 ; i++) {
     console.log('*'.repeat(5))
