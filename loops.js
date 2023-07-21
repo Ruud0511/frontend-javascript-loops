5271c20a4be86d42f80ddae8dd7e3eee343a9def
Ruud0511 <post@ruud.co>
2023-07-21 10:54:18 +0200

    FizzBuzz doorgelopen

--- loops.js
+++ loops.js
@@ -101,8 +101,22 @@ for (let i = 0; i < 10; i++) {
 // etc.
 // ==========================================
 
-for (let i = 0; i < 101; i++) {
+for (let i = 0; i < 101; i++)
+
+// veelvoud van 3 en 5
+{    if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz')
+}
+// veelvoud van 3
+   else if (i % 3 === 0) {
+    console.log('Fizz')
+    }
+
+// veelvoud van 5
+    else if (i % 5 === 0) {
+    console.log('Buzz')
+    }
+    else
     console.log(i);
 
 }
-
